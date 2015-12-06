91de6ac3d0e291e58db4081a29b39b5de4ed1e38
Miguel Espinoza <wiseit@outlook.com>
2015-12-06 10:56:55 +0100

    Click on a category takes you to the infoCard

--- public/js/jujube.js
+++ public/js/jujube.js
@@ -1,18 +1,22 @@
 window.jujube = window.jujube || {};
 
-
+/**
+ * App Settings
+ */
 jujube.settings = {
     location: '1',
     category: '1',
     baseUrl: window.location.href,
-    idleTimer: 3, // seconds
+    idleTimer: 15, // seconds
     isDebug: true
 };
 
 jujube.timer = {};
 
 jujube.setLocation = function setLocation() {};
-jujube.setCategory = function setCategory() {};
+jujube.setCategory = function setCategory() {
+    this.showView(undefined, 'infoCard');
+};
 
 jujube.init = function init() {
     // set up jquery ajax
@@ -23,6 +27,9 @@ jujube.init = function init() {
     var self = this;
 
     jujube.timer.lastAction = new Date();
+    if (typeof jujube.timer.loadRandom !== 'undefined') {
+        clearInterval(jujube.timer.loadRandom);
+    }
     jujube.timer.loadRandom = window.setInterval(function loadRandomfn() {
         window.console.log('-->');
         self.checkIdle.call(self);
@@ -31,6 +38,7 @@ jujube.init = function init() {
         this.settings.isDebug = false;
     }
 
+    jujube.getRandomCard();
     /*
         doenst work for me
         $('body').mousemove(self.updateIdle.call(self));
@@ -42,13 +50,25 @@ jujube.init = function init() {
         }, false);*/
     $('#navToSettings').bind('click touchstart', function bindClickSettings(e) { self.showView(e, 'settings'); });
     $('#navToInfoCard').bind('click touchstart', function bindClickInfo(e) { self.showView(e, 'infoCard'); });
+    $('#settings .selector').bind('click touchstart', function bindClickCategories(e) { self.setCategory(e); })
 };
 
+/**
+ * @brief Updates the timer to cycle through random cards
+ * @see jujube.checkIdle
+ * @details [long description]
+ * @return [description]
+ */
 jujube.updateIdle = function updateIdle() {
     window.console.log('Tracker');
     this.timer.lastAction = new Date();
 };
 
+/**
+ * @brief Requests a random card from the server using the category and location set.
+ * @details [long description]
+ * @return [description]
+ */
 jujube.getRandomCard = function getRandomCard() {
     $.ajax({
         url: this.settings.baseUrl + 'card/' + this.settings.location + '/' + this.settings.category,
@@ -56,6 +76,13 @@ jujube.getRandomCard = function getRandomCard() {
     });
 };
 
+/**
+ * @brief Updates the InfoCard with data fetched from the server
+ * @details [long description]
+ *
+ * @param  [description]
+ * @return [description]
+ */
 jujube.showCardInfo = function showCardInfo(response) {
     window.console.log('showCardInfo:', response);
     var card = $('#infoCard');
@@ -80,7 +107,9 @@ jujube.checkIdle = function checkIdle() {
 };
 
 jujube.showView = function showView(e, which) {
-    e.preventDefault();
+    if (typeof e !== 'undefined') {
+        e.preventDefault();
+    }
     window.console.log('showView(' + which + ')');
     $('.view').hide('slow', function onCompleteHideViews() {
         $('#' + which).show('slow');
