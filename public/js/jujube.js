906d8cd020f3d93a09e1037d02392679683f2604
Miguel Espinoza <wiseit@outlook.com>
2015-12-06 02:40:34 +0100

    Request a new random card after 15 seconds of inactivity

--- public/js/jujube.js
+++ public/js/jujube.js
@@ -4,8 +4,11 @@ window.jujube = window.jujube || {};
 jujube.settings = {
     location: '1',
     category: '1',
-    baseUrl: 'http://localhost/'
+    baseUrl: 'http://localhost/',
+    idleTimer: 15 // seconds
 };
+
+jujube.timer = {};
 
 jujube.setLocation = function setLocation() {};
 jujube.setCategory = function setCategory() {};
@@ -16,6 +19,13 @@ jujube.init = function init() {
         // url: 'http://jujube.mybluemix.net'
         dataType: 'json'
     });
+    var self = this;
+
+    jujube.timer.lastAction = new Date();
+    jujube.timer.loadRandom = window.setInterval(function loadRandomfn() {
+        window.console.log('-->');
+        self.checkIdle.call(self);
+    }, self.settings.idleTimer * 1000);
 };
 
 jujube.getRandomCard = function getRandomCard() {
@@ -31,7 +41,21 @@ jujube.showCardInfo = function showCardInfo(response) {
     card.find('.banner').attr('src', response['img_url']);
     card.find('#title, .person_name').html(response.title);
     card.find('.copy').html(response.text);
+    jujube.getSimilarCard(response.id);
+};
 
+jujube.getSimilarCard = function getSimilarCard(id) {
+    $.ajax({
+        url: this.settings.baseUrl + 'card/'
+    });
+};
+
+jujube.checkIdle = function checkIdle() {
+    var diff = (new Date()) - this.timer.lastAction;
+    if (diff > this.settings.idleTimer) {
+        this.timer.lastAction = new Date();
+        this.getRandomCard();
+    }
 };
 
 
