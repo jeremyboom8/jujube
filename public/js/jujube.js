75338e50c0ef3183d701b8ea496b21ef85f1cd81
Miguel Espinoza <wiseit@outlook.com>
2015-12-06 03:29:11 +0100

    Rename the wrapper for infoCard Quick implement of the settings view Ability to switch between settings and infoCard

--- public/js/jujube.js
+++ public/js/jujube.js
@@ -4,8 +4,8 @@ window.jujube = window.jujube || {};
 jujube.settings = {
     location: '1',
     category: '1',
-    baseUrl: window.location.href, //'http://localhost/',
-    idleTimer: 15, // seconds
+    baseUrl: window.location.href,
+    idleTimer: 3, // seconds
     isDebug: true
 };
 
@@ -30,6 +30,23 @@ jujube.init = function init() {
     if (window.location.href.indexOf('mybluemix') > (-1)) {
         this.settings.isDebug = false;
     }
+
+    /*
+        doenst work for me
+        $('body').mousemove(self.updateIdle.call(self));
+        $('body').bind('touchmove',self.updateIdle.call(self));
+        document.addEventListener('touchmove', function jsTouchMove(e) {
+            e.preventDefault();
+            var touch = e.touches[0];
+            window.console.log(touch.pageX + ' - ' + touch.pageY);
+        }, false);*/
+    $('#navToSettings').bind('click touchstart', function bindClickSettings(e) { self.showView(e, 'settings'); });
+    $('#navToInfoCard').bind('click touchstart', function bindClickInfo(e) { self.showView(e, 'infoCard'); });
+};
+
+jujube.updateIdle = function updateIdle() {
+    window.console.log('Tracker');
+    this.timer.lastAction = new Date();
 };
 
 jujube.getRandomCard = function getRandomCard() {
@@ -41,7 +58,7 @@ jujube.getRandomCard = function getRandomCard() {
 
 jujube.showCardInfo = function showCardInfo(response) {
     window.console.log('showCardInfo:', response);
-    var card = $('#wrapper');
+    var card = $('#infoCard');
     card.find('.banner').attr('src', response['img_url']);
     card.find('#title, .person_name').html(response.title);
     card.find('.copy').html(response.text);
@@ -56,12 +73,19 @@ jujube.getSimilarCard = function getSimilarCard(id) {
 
 jujube.checkIdle = function checkIdle() {
     var diff = (new Date()) - this.timer.lastAction;
-    if (diff > this.settings.idleTimer) {
+    if (diff > this.settings.idleTimer && $('#infoCard').is(':visible')) {
         this.timer.lastAction = new Date();
         this.getRandomCard();
     }
 };
 
+jujube.showView = function showView(e, which) {
+    e.preventDefault();
+    window.console.log('showView(' + which + ')');
+    $('.view').hide('slow', function onCompleteHideViews() {
+        $('#' + which).show('slow');
+    });
+};
 
 (function initializer() {
     'use strict';
